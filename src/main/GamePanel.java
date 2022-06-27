09960bb0026fb9700319459b7c827cdbe68da4ab
Nghialato <87885511+Nghialato@users.noreply.github.com>
2022-06-27 09:40:17 +0700

    Add files via upload

---
+++ src/main/GamePanel.java
@@ -0,0 +1,91 @@
+package main;
+import entity.Player;
+import tile.TileManager;
+
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements  Runnable{
+    final int originalTileSize = 16;
+    final int scale = 3;
+
+    public int tileSize = originalTileSize * scale;
+    public int maxScreenCol = 16;
+    public int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol;
+    public final int screenHeight = tileSize * maxScreenRow;
+
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldWidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxWorldRow;
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    public CollisionChecker cChecker = new CollisionChecker(this);
+    public Player player = new Player(this,keyH);
+
+
+
+
+    int FPS = 60 ;
+
+    TileManager tileM = new TileManager(this);
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+    public void startGameThread(){
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public void run(){
+        double drawInterval = 1000000000/FPS;
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+
+        while(gameThread != null){
+            long currentTime = System.nanoTime();
+
+            update();
+            repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000;
+
+                if(remainingTime <0){
+                    remainingTime = 0;
+                }
+                Thread.sleep((long) remainingTime);
+                nextDrawTime +=drawInterval;
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    public void update() {
+        if(keyH.downPressed || keyH.upPressed || keyH.leftPressed || keyH.rightPressed) {
+            player.update();
+        }
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        tileM.draw(g2);
+        player.draw(g2);
+
+        g2.dispose();
+
+    }
+}
