598f13b27fdfc3ca781bce59d02f7773e6950882
Nghialato <87885511+Nghialato@users.noreply.github.com>
2022-07-10 14:25:12 +0700

    Delete src directory

--- src/tile/Tile.java
+++
@@ -1,9 +0,0 @@
-package tile;
-
-import java.awt.image.BufferedImage;
-
-public class Tile {
-    protected BufferedImage image_center, image2, image1, image;
-    public boolean collision = true; // Check collision true or false
-    protected int explosion = 0; // 0: Nothing, 1: Can be explosion, 2: Can not be explosion
-}
